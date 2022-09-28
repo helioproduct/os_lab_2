059961144115c8aa81d9867031f4dfedd353d216
helio <helioproduct.sync@gmail.com>
2022-09-29 00:16:18 +0300

    some updates

--- src/main.c
+++ src/main.c
@@ -6,13 +6,12 @@
 char* read_string() 
 {
     int len = 0;
-    int capacity = 1; // 1, так как точно будет '\0'
+    int capacity = 1; 
     char *string = (char*) malloc(sizeof(char));
-
     char c = getchar();
     if (c == '\n') c = getchar();
     while (c != '\n') {
-        string[len++] = c; // заносим в строку новый символ
+        string[len++] = c;
         if (len >= capacity) {
             capacity *= 2;
             string = (char*) realloc(string, capacity * sizeof(char));
@@ -23,14 +22,36 @@ char* read_string()
     return string;
 }
 
+// int test(void)
+// {
+//     char *string = read_string();
+//     void *source = fopen(string, "r");
+
+//     if (source == NULL) {
+//         perror("File open error\n");
+//         return 1;
+//     }
+
+//     stdin = source;
+//     char c = getchar();
+//     while (c != EOF) {
+//         printf("%c", c);
+//         c = getchar();
+//     }    
+
+//     return 0;
+// }
+
 int main(void)
 {
+    int number = 1;
+
+
     int id = fork();
     if (id == -1) {
         perror("Fork error\n");
         return 1;
     }
-
     int fd[2];
     if (pipe(fd) == -1) {
         perror("Pipe error\n");
@@ -38,15 +59,22 @@ int main(void)
     }
     // child process
     if (id == 0) {
+        close(fd[1]); // close write
         
-    
+        int value_from_parent;
+        read(fd[0], &value_from_parent, sizeof(int));
+        
+        printf("got %d from parent\n", value_from_parent);
+        
+        close(fd[0]);
     }
     // parent process
     else {
-        char *string = read_string();
-        int pointer_value = (long) fopen(string, "r");
-
+        close(fd[0]); // close read
+        // char *path_to_file = read_string();
+        write(fd[1], &number, sizeof(int));
+        close(fd[1]);
+        wait(NULL);
     }
-
     return 0;
 }
